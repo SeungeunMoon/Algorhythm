92675b94a103a5afcf8cc15250d5364ce1806077
estiemoon <102018454+SeungeunMoon@users.noreply.github.com>
2026-08-04 16:36:57 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,6 +1,8 @@
-select apnt_no, pt_name, p.pt_no, d.mcdp_cd, dr_name, apnt_ymd
-from appointment as a
-join patient as p on a.pt_no = p.pt_no
-join doctor as d on a.mddr_id = d.dr_id
-where apnt_ymd like '2022-04-13%' and APNT_CNCL_YN = 'N'
-order by apnt_ymd
+select a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
+from appointment a
+join patient p on a.pt_no = p.pt_no
+join doctor d on a.mddr_id = d.dr_id
+where substr(a.apnt_ymd,1,10) = '2022-04-13'
+and a.apnt_cncl_yn = 'N'
+and a.mcdp_cd = 'CS'
+order by a.apnt_ymd
